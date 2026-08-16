5ba69787d5ef2f46f929513d1f83ce20a2f7b212
Jhonatan Zago <jhonatanzago10@gmail.com>
2026-08-16 09:13:06 -0300

    first commit

---
+++ src/test/java/com/example/carteirainvestimento/CarteiraInvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carteirainvestimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraInvestimentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
